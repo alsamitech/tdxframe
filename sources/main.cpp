17eff00a3fbb0a6d20de3f55775499e0a7bc6985
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-14 04:45:27 +0000

    updated a few things

---
+++ sources/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "uv.h"
+
+int main() {
+
+}
---
+++ sources/uv.h
@@ -0,0 +1,14 @@
+int a1;
+int a2;
+int a3;
+int a4;
+int a5;
+int a6;
+int a7;
+int a8;
+int a9;
+int a10;
+double a11;
+double a12;
+double a13;
+double a14;
