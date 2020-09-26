66de27ec0661537507a856070143ae30a7e8bed0
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-26 06:27:15 +0000

    Applied the exC math update to the standard version of the framework

--- sources/exmath.h
+++ sources/exmath.h
@@ -29,6 +29,15 @@ void ipw(int baseyu, int powyu) {
   }
   ipx = resl;
 }
+int ssq(int ssqd, int ssqx) {
+  int ssyn = (ssgd * ssqx) - ssqx + (ssqx/2);
+  return ssyn;
+}
+
+template <class FNUM_SMB, class SNUM_SMB>
+FNUM_SMB smaller(FNUM_SMB FIRSTCMP_00, SNUM_SMB SECMP_00) {return(FIRSTCMP_00 < SECMP_00? FIRSTCMP_00:SECMP_00);}
+FNUM_SMB larger(FNUM_SMB FIRSTCMP_01, SNUM_SMB SECMP_01) {return(FIRSTCMP_01 > SECMP_01? FIRSTCMP_01:SECMP_01);}
+
 /*
 This is part of the EXMATH library. 
 */
